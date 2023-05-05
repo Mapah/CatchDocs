dafb293ad30950b7f99f5696b2ede949c331da76
Fopa-08 <ranitafopa9@gmail.com>
2023-05-05 13:23:59 +0100

    ajout des modifications

--- app/Http/Controllers/Api/DocumentsController.php
+++ app/Http/Controllers/Api/DocumentsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Documents;
+// use Dotenv\Validator;
+// use Illuminate\Contracts\Validation\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class DocumentsController extends Controller
@@ -22,5 +24,44 @@ class DocumentsController extends Controller
         ]);
         return response($data, Response::HTTP_CREATED);
     }
+
+    public function FileStore(Request $request)
+    {
+        // $validator =  Validator::make($request->all(),[
+        //     'documents' => 'required|mimes:pdf|max:5000',
+        //     'description'=> 'required|max:2000',
+        //     'name'=> 'required|max:2000',
+        //     'offices_id'=> 'required|max:2000',
+        //     'title'=> 'required|max:2000',
+        //     'source'=> 'required|max:2000',
+        //     'text'=> 'required|max:2000',
+        //     'tags'=> 'required|max:2000',
+        //     'annotations'=> 'required|max:2000',
+        // ]);
+       
+        // if ($validator->fails())
+        //  {
+        //     return response()->json($validator->failed(),400);
+        //  }
+      
+
+         $documents_path = $request->file('documents')->store('documents', 'public');
+       
+
+         $data = Documents::create([
+             'name'=> $request->name,
+             'description' => $request->description,
+             'path' => $documents_path,
+             'offices_id'=>$request->offices_id, 
+             'title'=> $request->documents_title,
+             'source'=> $request->documents_source,
+             'text'=> $request->documents_text,
+             'tags'=> $request->documents_tags,
+             'annotations'=> $request->documents_annotations,
+         ]);
+
+         return response()->json(["message"=>"document has been created successfully",$data],200);
+     }
+
 }
 
